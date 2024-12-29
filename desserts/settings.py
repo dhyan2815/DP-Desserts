26034d06b7deccd8b6e0df44db04eb342a364c89
dhyan2004 <liomad999@gmail.com>
2024-12-29 16:37:28 +0530

    debug settings updated

--- desserts/settings.py
+++ desserts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1q1za*5)_(5eo=2m7t*_ne)f55@zz-ur2nsw#m#s4(1gbfrb9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 'dp-desserts.onrender.com' , '127.0.0.1', 'localhost' ]
 
